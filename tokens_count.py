df4817502e5b70b0b1e7ad9edb5e39da3d07901c
Tien Dung <dungtn@gmail.com>
2024-10-01 18:27:04 +0700

    update

--- tokens_count.py
+++ tokens_count.py
@@ -124,7 +124,9 @@ print(len(tid_count_pairs))
 
 tid_count_pairs.sort( key = lambda x: -x[1] )
 
-x = tid_count_pairs[: 100] + tid_count_pairs[-100 : ]
+x = tid_count_pairs[      :   100] + 
+    tid_count_pairs[-1100 : -1000] +
+    tid_count_pairs[ -100 :      ]
 
 maxx = 25
 spaces = " " * 100
