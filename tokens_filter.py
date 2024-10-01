6757e500674f0a1faee53cfa24f1513ef009ccd8
Tien Dung <dungtn@gmail.com>
2024-10-01 14:21:25 +0700

    update

--- tokens_filter.py
+++ tokens_filter.py
@@ -15,28 +15,50 @@ tokenizer = AutoTokenizer.from_pretrained(
     model_max_length = 1024 * 1024 * 4, # 4m ctxlen should fit a book
 )
 
+
+def count_tokens(texts):
+    count = {}
+    for text in texts:
+        # tokens = tokenizer.tokenize(text)
+
+        token_ids = tokenizer.encode(text)
+        # text = tokenizer.decode(token_ids)
+
+        for tid in token_ids:
+
+            if tid not in count:
+                count[tid] = 0
+
+            count[tid] += 1
+    return count
+
+
+
+def merge_count(count, x):
+    for k, v in x.items():
+
+        if k not in count:
+            count[k] = 0
+
+        count[k] += v
+
+
 def get_uniq_tokens(infile):
     outfile = infile + "_count.json"
 
     if not os.path.exists(outfile):
 
         print(f"get_uniq_token {infile} ...")
+
         count = {}
+        texts =  [ json.loads(line)["text"] for line in lzma.open(infile) ]
 
-        for line in lzma.open(infile):
+        chunk_size = 1024
+        chunks = [texts[i:i + chunk_size] for i in range(0, len(texts), chunk_size)]
 
-            text = json.loads(line)["text"]
-            # tokens = tokenizer.tokenize(text)
-
-            token_ids = tokenizer.encode(text)
-            # text = tokenizer.decode(token_ids)
-
-            for tid in token_ids:
-
-                if tid not in count:
-                    count[tid] = 0
-
-                count[tid] += 1
+        with Pool(processes = num_procs()) as pool:
+            for x in pool.imap_unordered(count_tokens, chunks):
+                merge_count(count, x)
 
         with open(outfile, "wt") as f:
             f.write(json.dumps(count))
@@ -49,19 +71,11 @@ def get_final_count():
 
     if not os.path.exists(countfile):
 
-        n = num_procs()# if num_procs() < len(input_files) else len(input_files)
         count = {}
 
-        with Pool(processes = n) as pool:
-
-            for x in pool.imap_unordered(get_uniq_tokens, input_files):
-
-                for k, v in x.items():
-
-                    if k not in count:
-                        count[k] = 0
-
-                    count[k] += v
+        for infile in input_files:
+            x = get_uniq_tokens(infile)
+            merge_count(count, x)
 
         with open(countfile, "wt") as f:
             f.write(json.dumps(count))
