16b3a994ee268ad778c9de4c6ab9261dfea8eaaf
Tien Dung <dungtn@gmail.com>
2024-10-01 16:43:36 +0700

    update

--- tokens_count.py
+++ tokens_count.py
@@ -64,7 +64,7 @@ def get_uniq_tokens(infile):
         text = json.loads(line)["text"]
         texts.append( text )
 
-        if idx % 128 == 127:
+        if idx % 1024 == 1023:
             merge_count(count, count_tokens(texts))
             count["last_line_idx"] = idx
 
