a5820899e47edf7b99b33c3c2175d96a77b41dd8
Your Name <you@example.com>
2024-10-07 20:19:27 +0000

    update

--- finetune_qwen__3_final.sh
+++ finetune_qwen__3_final.sh
@@ -10,14 +10,14 @@ python finetune.py \
   --finetune_layers "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25" \
   --data_path "$data_path" \
   --model_max_length 4096 \
-  --output_dir "../Qwen2.5-1.5B-Instruct__trimm_vocab__final" \
+  --output_dir "../Qwen2.5-7B-Instruct__trimm_vocab__final" \
   --num_train_epochs 3 \
   --per_device_train_batch_size 1 \
   --gradient_accumulation_steps 16 \
-  --logging_steps 3 \
+  --logging_steps 2 \
   --save_strategy "steps" \
-  --save_steps 300 \
-  --save_total_limit 2 \
+  --save_steps 200 \
+  --save_total_limit 1 \
   --learning_rate 5e-5 \
   --warmup_ratio 0.01 \
   --lr_scheduler_type "linear" \
