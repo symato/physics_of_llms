ea646193d12972b9edee8aa5c50c6e610b4358b2
Your Name <you@example.com>
2024-10-07 13:03:24 +0000

    40g vram can nearly full finetune qwen 2.5 7b @ 4k ctxlen

--- finetune_qwen__3_final.sh
+++ finetune_qwen__3_final.sh
@@ -3,11 +3,11 @@
 export PACK_DATA=1
 
 data_path=final_finetune
-rm -rf data_cached/$data_path
+#rm -rf data_cached/$data_path
 
 python finetune.py \
   --model_name_or_path "../Qwen2.5-7B-Instruct__trimm_vocab" \
-  --finetune_layers "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26" \
+  --finetune_layers "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25" \
   --data_path "$data_path" \
   --model_max_length 4096 \
   --output_dir "../Qwen2.5-1.5B-Instruct__trimm_vocab__final" \
@@ -18,7 +18,7 @@ python finetune.py \
   --save_strategy "steps" \
   --save_steps 300 \
   --save_total_limit 2 \
-  --learning_rate 1e-5 \
+  --learning_rate 5e-5 \
   --warmup_ratio 0.01 \
   --lr_scheduler_type "linear" \
   --report_to "none" \
